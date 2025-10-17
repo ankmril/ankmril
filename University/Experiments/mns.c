c8aa389d02d02c3fe0bd973fc04ec19cd0459398
ivibewithghosts <ankmril9@gmail.com>
2025-10-17 14:09:28 +0530

    initial code, fixed to perfection

--- University/Experiments/mns.c
+++ University/Experiments/mns.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int r1,c1,r2,c2,i,j,sm,ss,n;
+    int r1=0,c1=0,i=0,j=0,sm=0,ss=0;
+
     printf("Enter the rows and columns of first matrix:\n");
     scanf("%d%d",&r1,&c1);
-    printf("Enter the rows and columns of second matrix:\n");
-    scanf("%d%d",&r2,&c2);
-    printf("Enter the elements of array:\n");
-    int arr[n];
-    for(i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-    for(i=0;i<n;i++) {
-        for(j=0;j<n;j++) {
-            if(i==j) {
-                if(arr[i]==arr[j]) {
-                    sm+=arr[i];
-                }
-            }
+
+    int arr[r1][c1];
+
+    printf("Enter the elements of first matrix:\n");
+
+    for(i=0;i<r1;i++) {
+        for(j=0;j<c1;j++)
+            scanf("%d",&arr[i][j]);
+    }
+
+    for(i=0;i<r1;i++) {
+        for(j=0;j<c1;j++) {
+            if(i==j)
+                sm+=arr[i][j];
+            if((i+j)==(r1-1))
+                ss+=arr[i][j];
         }
     }
+
+    printf("Sum of main diagonal elements: %d\n",sm);
+    printf("Sum of secondary diagonal elements: %d\n",ss);
+
     return 0;
 }
