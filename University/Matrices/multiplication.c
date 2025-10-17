3470e5ea6d0446910c8cfbcbdf27b8e03a8cebd9
ivibewithghosts <ankmril9@gmail.com>
2025-10-17 07:24:12 +0530

    fix lines of code

--- University/Matrices/multiplication.c
+++ University/Matrices/multiplication.c
@@ -12,29 +12,25 @@ int main() {
     int a[r1][c1],b[r2][c2],c[r1][c2];
     printf("Enter the elements of the first matrix\n");
     for(i=0;i<r1;i++) {
-        for(j=0;j<c1;j++) {
+        for(j=0;j<c1;j++)
             scanf("%d",&a[i][j]);
-        }
     }
     printf("Enter the elements of the second matrix\n");
     for(i=0;i<r2;i++) {
-        for(j=0;j<c2;j++) {
+        for(j=0;j<c2;j++)
             scanf("%d",&b[i][j]);
-        }
     }
     for(i=0;i<r1;i++) {
         for(j=0;j<c2;j++) {
             c[i][j]=0;
-            for(k=0;k<c1;k++) {
+            for(k=0;k<c1;k++)
                 c[i][j]+=a[i][k]*b[k][j];
-            }
         }
     }
     printf("The resultant matrix after multiplication:\n");
     for(i=0;i<r1;i++) {
-        for(j=0;j<c2;j++) {
+        for(j=0;j<c2;j++)
             printf("%d ",c[i][j]);
-        }
         printf("\n");
     }
     return 0;
