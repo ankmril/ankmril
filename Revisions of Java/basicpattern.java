eec5617fbcfb5a0c8b1c7b90ca00d6a96b733ad3
ivibewithghosts <ankmril9@gmail.com>
2025-10-19 13:42:18 +0530

    started a new java rev folder

---
+++ Revisions of Java/basicpattern.java
@@ -0,0 +1,12 @@
+public class basicpattern {
+    public void main(String args[]) {
+        int x,y;
+        
+        for(x=1;x<=5;x++) {
+            for(y=1;y<=x;y++) {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+}
