4b1400f0d0ee6113c74f7219041a2dbf8b588f49
ivibewithghosts <ankmril9@gmail.com>
2025-10-19 16:40:33 +0530

    modify code

--- Learning Rust/Harkirat Singh's Tutorial/rust-proj/src/main.rs
+++ Learning Rust/Harkirat Singh's Tutorial/rust-proj/src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let x = -5;
-    let y: u32 = 1000;
-    let z: f32 = 1000.001;
-    println!("Hello, world!");
-    println!("{}\n{}\n{}",x,y,z);
+    let mut x = 5;
+    x = x + 1;
+    println!("{}",x);
 }
