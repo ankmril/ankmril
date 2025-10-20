525ffc03eceed422925c37cb4286e66dcd4131e3
ivibewithghosts <ankmril9@gmail.com>
2025-10-20 23:10:08 +0530

    learning terminal and nvim

--- rust/harkirat/rust-proj/src/main.rs
+++ rust/harkirat/rust-proj/src/main.rs
@@ -4,12 +4,16 @@ fn main() {
     println!("Enter a number below and I will guess which number you typed :)");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input.");
     
-    let number: i32 = input
+    let mut number: i32 = input
         .trim()
         .parse()
         .expect("Please enter a valid number.");
     
-    println!("\nYou sneaky boy I knew you would type {}. haha :)\n", number);
+    number = number + 1;
+
+    println!("\nYou sneaky boy I knew you would type whatever comes before {}. haha :)\n", number);
 }
