a09f513180d42eff950f0e26397e7c90f0bddcc9
ivibewithghosts <ankmril9@gmail.com>
2025-10-20 19:01:24 +0530

    testing terminal cmds

--- rust/harkirat/rust-proj/src/main.rs
+++ rust/harkirat/rust-proj/src/main.rs
@@ -1,15 +1,17 @@
 use std::io;
 
 fn main() {
-    println!("Enter a number:");
+    println!("Enter a number to be printed with an addition of 5:");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input.");
     
-    let number: i32 = input
+    let mut number: i32 = input
         .trim()
         .parse()
         .expect("Please enter a valid number.");
+
+    number = number + 5;
     
     println!("\nYou entered the number {}.\n", number);
 }
