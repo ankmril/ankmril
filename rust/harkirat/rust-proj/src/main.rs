3d7bddb869d449882841d285ffee91077c2dce56
ivibewithghosts <ankmril9@gmail.com>
2025-10-20 19:36:28 +0530

    demonstrating the terminal aliases

--- rust/harkirat/rust-proj/src/main.rs
+++ rust/harkirat/rust-proj/src/main.rs
@@ -1,17 +1,15 @@
 use std::io;
 
 fn main() {
-    println!("Enter a number to be printed with an addition of 5:");
+    println!("Enter a number below and I will guess which number you typed :)");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input.");
     
-    let mut number: i32 = input
+    let number: i32 = input
         .trim()
         .parse()
         .expect("Please enter a valid number.");
-
-    number = number + 5;
     
-    println!("\nYou entered the number {}.\n", number);
+    println!("\nYou sneaky boy I knew you would type {}. haha :)\n", number);
 }
