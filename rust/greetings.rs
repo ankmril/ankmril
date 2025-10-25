8a002040843f7fa164ad0b40036b7970ef1603e1
ivibewithghosts <ankmril9@gmail.com>
2025-10-25 17:41:11 +0530

    practice question: learning string operations | greetings

---
+++ rust/greetings.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let mut name: String = String::new(); // initialize input
+
+    println!("enter your name:");
+    io::stdin() // "use std::io;"" followed by this to input
+        .read_line(&mut name) // syntax for input
+        .expect("failed to read line"); // always write this
+
+    let name = name // let name to store trim of mut name
+        .trim();
+
+    println!("hii {}, how's your day today?", name);
+    println!("you've got a great weekend ahead of you, {}!", name);
+}
