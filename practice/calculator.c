87d2d7b96ba6db8a7678abd79adfb4ee47d90465
ankmril <ankmril.dev@gmail.com>
2025-11-13 21:12:58 +0530

    tidy code

--- practice/calculator.c
+++ practice/calculator.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 
 float result(char chr, float a, float b) {
+  float rs=0.0;
 
-    float rs=0.0;
-    if(chr=='/'&&b==0) {
-        printf("Error: Division by zero not allowed.\n");
-        return 0.0;
-    }
+  if(chr=='/'&&b==0) {
+    printf("Error: Division by zero not allowed.\n");
+    return 0.0;
+  }
 
-    switch(chr) {
-        case '+': rs=a+b; break;
-        case '-': rs=a-b; break;
-        case '*': rs=a*b; break;
-        case '/': rs=a/b; break;
-        default: printf("Invalid operator.\n");
-            return 0.0;
-    }
+  switch(chr) {
+    case '+': rs=a+b; break;
+    case '-': rs=a-b; break;
+    case '*': rs=a*b; break;
+    case '/': rs=a/b; break;
+    default: printf("Invalid operator.\n");
+      return 0.0;
+  }
 
-    return rs;
+  return rs;
 }
 
 int main() {
+  char chr;
+  float a,b;
+  int c;
 
-    char chr;
-    float a,b;
-    int c;
+  do {
+    printf("Enter the operation (+, -, *, /)\n");
+    scanf(" %c",&chr);
 
-    do {
-        printf("Enter the operation (+, -, *, /)\n");
-        scanf(" %c",&chr);
-        printf("Enter the two numbers\n");
-        scanf(" %f %f",&a,&b);
-        printf("Result: %f\n",result(chr,a,b));
-        printf("Do you want to perform another operation? (Enter 1 for yes and 2 for no.)\n");
-        scanf("%d",&c);
-        if(c!=1&&c!=2)
-        printf("Invalid input.\n");
-    } while(c==1);
+    printf("Enter the two numbers\n");
+    scanf(" %f %f",&a,&b);
 
-    if(c==2)
-        printf("Thanks for using the calculator.\n");
+    printf("Result: %f\n",result(chr,a,b));
+
+    printf("Do you want to perform another operation? (Enter 1 for yes and 2 for no.)\n");
+    scanf("%d",&c);
+    
+    if(c!=1&&c!=2)
+      printf("Invalid input.\n");
+  } while(c==1);
+
+  if(c==2)
+    printf("Thanks for using the calculator.\n");
         
-    return 0;
+  return 0;
 }
