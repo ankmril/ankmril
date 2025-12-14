e1408070b28b603c3da7a461945b858b6dcb96ac
ankmril <ankmril.dev@gmail.com>
2025-12-14 09:27:59 +0530

    fix code

--- university/assignments/largestnumberif.c
+++ university/assignments/largestnumberif.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+
 int main() {
-    int a,b,c,d;
-    printf("Enter three numbers to be checked for the largest one\n");
-    scanf("%d%d%d",&a,&b,&c);
-    if(a==b==c) {
-        goto end;
+  int a, b, c, d = 0;
+
+  printf("Enter three numbers:\n");
+  scanf(" %d %d %d", &a, &b, &c);
+
+  if(!(a == b && b == c && c == a)) {
+    if(a > b && a > c) {
+      d = a;
     }
-    if(a>b&&a>c)
-    printf("Largest value: %d",a);
-    if(b>c&&b>a)
-    printf("Largest value: %d",b);
-    if(c>a&&c>b)
-    printf("Largest value: %d",c);
-    end:
-    printf("Input values are equal. Try again.\n");
-    return 0;
+    if(b > a && b > c) {
+      d = b;
+    }
+    if(c > a && c > b) {
+      d = c;
+    }
+    printf("%d is the largest number.\n", d);
+  } else {
+    printf("Given three numbers are equal.\n");
+  }
+
+  return 0;
 }
